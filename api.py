7fa1015e1f3bd55ba0d50aa12d5b81c66c4c5d88
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-08-31 22:29:44 -0400

    Modulando, en este modulo solo estarà la conexion a la API

---
+++ api.py
@@ -0,0 +1,16 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+def get_apikey():
+    load_dotenv()
+    API_key = os.getenv('API_key')
+    return API_key
+
+def get_weather(city):
+    api_key = get_apikey()
+    url =  f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    return response
+
