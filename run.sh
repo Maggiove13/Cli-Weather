67beb23e462bfba1a545f903535320203e42d99d
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-09-01 21:30:44 -0400

    Automatizacion de tareas

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Comprobacion si el entorno virtual ya existe
+if [ ! -d "venv" ]; then
+  echo "Creando entorno virtual..."
+  python -m venv venv
+fi
+
+# Activar el entorno virtual
+source venv/bin/activate
+
+# Instalar dependencias desde requirements.txt
+echo "Instalando dependencias..."
+pip install -r requirements.txt
+
+# Ejecutar la aplicacion con parametros predeterminados
+echo "Ejecutando la aplicacion..."
+python main.py -location "Madrid" -format json
+
+echo "-Script completado!"
+
